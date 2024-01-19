7ad9a00e1ccd76900720e564c27689eb247982be
tingzhao.ytz <yutingzhao1991@gmail.com>
2024-01-19 19:56:32 +0800

    feat: add goerli test contract

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,15 +1,26 @@
 import { createConfig, http, useReadContract, useWriteContract } from "wagmi";
-import { mainnet } from "wagmi/chains";
-import { WagmiWeb3ConfigProvider, MetaMask } from "@ant-design/web3-wagmi";
-import { Address, NFTCard, Connector, ConnectButton } from "@ant-design/web3";
+import { mainnet, goerli } from "wagmi/chains";
+import {
+  WagmiWeb3ConfigProvider,
+  MetaMask,
+  Goerli,
+} from "@ant-design/web3-wagmi";
+import {
+  Address,
+  NFTCard,
+  Connector,
+  ConnectButton,
+  useAccount,
+} from "@ant-design/web3";
 import { injected } from "wagmi/connectors";
 import { Button, message } from "antd";
 import { parseEther } from "viem";
 
 const config = createConfig({
-  chains: [mainnet],
+  chains: [mainnet, goerli],
   transports: {
     [mainnet.id]: http(),
+    [goerli.id]: http(),
   },
   connectors: [
     injected({
@@ -19,18 +30,21 @@ const config = createConfig({
 });
 
 const CallTest = () => {
+  const { account } = useAccount();
   const result = useReadContract({
     abi: [
       {
         type: "function",
-        name: "totalSupply",
+        name: "balanceOf",
         stateMutability: "view",
-        inputs: [],
-        outputs: [{ name: "supply", type: "uint256" }],
+        inputs: [{ name: "account", type: "address" }],
+        outputs: [{ type: "uint256" }],
       },
     ],
+    // Goerli test contract 0x418325c3979b7f8a17678ec2463a74355bdbe72c
     address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
-    functionName: "totalSupply",
+    functionName: "balanceOf",
+    args: [account?.address as `0x${string}`],
   });
   const { writeContract } = useWriteContract();
 
@@ -80,7 +94,11 @@ const CallTest = () => {
 
 export default () => {
   return (
-    <WagmiWeb3ConfigProvider config={config} wallets={[MetaMask()]}>
+    <WagmiWeb3ConfigProvider
+      config={config}
+      chains={[Goerli]}
+      wallets={[MetaMask()]}
+    >
       <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
       <NFTCard
         address="0x79fcdef22feed20eddacbb2587640e45491b757f"
